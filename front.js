9cfd4a71e375688aee409803f4dadf9143bc3f36
Sam Jiang <129788706+SamJiang47@users.noreply.github.com>
2023-06-03 20:37:08 -0700

    fix errors and functional improvement

--- front.js
+++ front.js
@@ -1,5 +1,9 @@
-
 const errorMessageElement = document.getElementById("errorMessage");
+var canvas = document.getElementsByClassName("canvas")[0];
+const errorElement1 = document.getElementById("error1");
+const errorElement2 = document.getElementById("error2");
+const select1 = document.getElementById("From");
+const select2 = document.getElementById("To");
 
 function changeFormat(body) {
   const xyValues = [];
@@ -14,17 +18,21 @@ function changeFormat(body) {
 }
 
 function checkSelections() { //conditional error display function
-  const select1 = document.getElementById("From");
-  const select2 = document.getElementById("To");
-  const errorElement1 = document.getElementById("error1");
-  const errorElement2 = document.getElementById("error2");
   if (select1.value === select2.value ){
   errorElement1.style.display = "block"; 
   errorElement2.style.display = "block";
+  document.getElementById('result1').innerHTML = ``;
+  document.getElementById('result2').innerHTML = ``;
+  canvas.style.display = "none";
+  myChart.style.display = "none";
   }
   else if (select1.value === "" || select2.value === "" ){
   errorElement1.style.display = "none"; 
   errorElement2.style.display = "block";
+  document.getElementById('result1').innerHTML = ``;
+  document.getElementById('result2').innerHTML = ``;
+  canvas.style.display = "none";
+  myChart.style.display = "none";
   }
   else {
   errorElement1.style.display = "none";
@@ -61,12 +69,19 @@ function resetInput() {
   document.getElementById("Amount2").value = ""; //Reset function for type in texts
   document.getElementById("From").value = ""; //Reset function for From
   document.getElementById("To").value = ""; //Reset function for To
+  canvas.style.display = "none";
+  myChart.style.display = "none";
+  document.getElementById('result1').innerHTML = ``;
+  document.getElementById('result2').innerHTML = ``;
 }
 
 const btn1 = document.getElementsByClassName("Convert");
 for (let i = 0; i < btn1.length; i++) {
   btn1[i].addEventListener('click',function(){  
   //convert amount
+    if (select1.value === select2.value ){
+      return;
+    }
     const target = document.getElementById('To').value;
     const base = document.getElementById('From').value;
     const amo = document.getElementById("Amount2");
@@ -76,7 +91,8 @@ for (let i = 0; i < btn1.length; i++) {
     xhr1.send(); 
     xhr1.onload = function(){ //Once we get response
       const body = JSON.parse(xhr1.responseText)  //Transfer from JSON format
-      document.getElementById('result').innerHTML = `${amo1}${base}=${body.result}${target}`;
+      document.getElementById('result1').innerHTML = `${amo1}${base}=${body.result}${target}`;
+      document.getElementById('result2').innerHTML = `${base} to ${target}`;
     }
   })
  }
@@ -84,6 +100,13 @@ for (let i = 0; i < btn1.length; i++) {
  const btn7 = document.getElementsByClassName("Trends7d");
  for (let i = 0; i < btn7.length; i++) {
      btn7[i].addEventListener('click', function () {
+         if (select1.value === select2.value ){
+         canvas.style.display = "none";
+         myChart.style.display = "none";
+         return;
+         }
+         canvas.style.display = "block";
+         myChart.style.display = "block";
          const target = document.getElementById('To').value;
          const base = document.getElementById('From').value;
          console.log(`Base: ${base}`);
@@ -98,7 +121,6 @@ for (let i = 0; i < btn1.length; i++) {
              let yValues = xyValues.map(obj => obj.y);
              let minValue = Math.min(...yValues);
              let maxValue = Math.max(...yValues);
-           
              new Chart("myChart", {
                 type: "scatter",
                 data: {
@@ -142,6 +164,13 @@ for (let i = 0; i < btn1.length; i++) {
  const btn3 = document.getElementsByClassName("Trends30d");
  for (let i = 0; i < btn3.length; i++) {
      btn3[i].addEventListener('click', function () {
+         if (select1.value === select2.value ){
+         canvas.style.display = "none";
+         myChart.style.display = "none";
+         return;
+         }
+         canvas.style.display = "block";
+         myChart.style.display = "block";
          const target = document.getElementById('To').value;
          const base = document.getElementById('From').value;
          const xhr3 = new XMLHttpRequest(); //Define XMLhttp object
